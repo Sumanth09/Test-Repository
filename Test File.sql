3e314f51f0744c106545a299d52fd07bfcb62add
sgopavaram <sgopavaram@INHYNSGOPAVARA1>
2018-11-14 16:00:13 +0530

    Test File

---
+++ Test File.sql
@@ -0,0 +1,20 @@
+USE [Test Database]
+GO
+
+/****** Object:  Table [dbo].[Table_1]    Script Date: 11/14/2018 3:59:52 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Table_1](
+	[a] [nchar](10) NULL,
+	[b] [nchar](10) NULL,
+	[c] [nchar](10) NULL,
+	[d] [nchar](10) NULL,
+	[e] [nchar](10) NULL
+) ON [PRIMARY]
+
+GO
+
